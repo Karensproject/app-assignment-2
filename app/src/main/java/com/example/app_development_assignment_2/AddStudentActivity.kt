aff99b8b4e2100299143b78e796db1b64ff12aae
karensproject <karenitz2708@gmail.com>
2026-01-29 20:06:41 +0200

    fix(setttings): settings bugs

--- app/src/main/java/com/example/app_development_assignment_2/AddStudentActivity.kt
+++ app/src/main/java/com/example/app_development_assignment_2/AddStudentActivity.kt
@@ -12,30 +12,30 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.app_development_assignment_2.model.Model
 import com.example.app_development_assignment_2.model.Student
 
-class AddNewStudentActivity : AppCompatActivity() {
+class AddStudentActivity : AppCompatActivity() {
     private lateinit var nameTextField: EditText
     private lateinit var idTextField: EditText
     private lateinit var phoneTextField: EditText
     private lateinit var addressTextField: EditText
-    private lateinit var isSelectedCheckBox: CheckBox
+    private lateinit var isCheckedCheckBox: CheckBox
     private lateinit var saveButton: Button
     private lateinit var cancelButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_new_student)
+        setContentView(R.layout.activity_add_student)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        idTextField = findViewById(R.id.add_student_activity_id_edit_text)
         nameTextField = findViewById(R.id.add_student_activity_name_edit_text)
+        idTextField = findViewById(R.id.add_student_activity_id_edit_text)
         phoneTextField = findViewById(R.id.add_student_activity_phone_edit_text)
         addressTextField = findViewById(R.id.add_student_activity_address_edit_text)
-        isSelectedCheckBox = findViewById(R.id.add_student_activity_checked_checkbox)
+        isCheckedCheckBox = findViewById(R.id.add_student_activity_checked_checkbox)
         saveButton = findViewById(R.id.add_student_activity_save_button)
         cancelButton = findViewById(R.id.add_student_activity_cancel_button)
         saveButton.setOnClickListener(::onSave)
@@ -47,9 +47,9 @@ class AddNewStudentActivity : AppCompatActivity() {
         val id = idTextField.text.toString()
         val phone = phoneTextField.text.toString()
         val address = addressTextField.text.toString()
-        val isSelected = isSelectedCheckBox.isChecked
+        val isChecked = isCheckedCheckBox.isChecked
 
-        val student = Student(name, id, phone, address, isSelected)
+        val student = Student(name, id, phone, address, isChecked)
         Model.shared.students.add(student)
 
         finish()
